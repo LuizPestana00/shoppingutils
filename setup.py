b7091780ac7b4dbd185f097f5b042e25552a1ef5
Luiz Pestana <felipe.pestana@edu.abrantes.pt>
2025-06-06 16:38:34 +0100

    updates

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
-from distutils.core import setup
-setup(name='shoppingutils',
-      version='3.0',
-      description='Shoppingutils Distribution Package',
-      author='Luiz Pestana',
-      author_email='aluno82329@ipt.pt',
-      url='https://github.com/LuizPestana00/shoppingutils',
-      packages=['shoppingutils'],
-     )
+from codecs import open
+from os.path import join, abspath, dirname
+from setuptools import setup, find_packages
+import os
+
+requirementPath = 'requirements.txt'
+install_requires = [] 
+
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+here = abspath(dirname(__file__))
+
+# Get the long description from the README file
+with open(join(here, 'README.md'), encoding='utf-8') as buff:
+    long_description = buff.read()
+    
+setup(
+    name="shoppingutils",
+    version="0.1",
+    description="Shoppingutils Distribution Package",
+    long_description=long_description,
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    include_package_data=True,
+    install_requires=install_requires
+)
